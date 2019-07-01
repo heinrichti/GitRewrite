b5b680eb67d14b9361b744095b92bda6e89ee2f9
Tim Heinrich <tim.heinrich.de@gmail.com>
2019-07-01 17:57:49 +0200

    Removed not required AsSpan

--- GitRewrite/GitObjects/ByteArrayExtensions.cs
+++ GitRewrite/GitObjects/ByteArrayExtensions.cs
@@ -1,11 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace GitRewrite.GitObjects
 {
-    static class SpanExtensions
+    static class ByteArrayExtensions
     {
+        public static bool SequenceEquals(this byte[] b1, byte[] b2)
+        {
+            if (b1.Length != b2.Length)
+                return false;
+
+            for (int i = b1.Length - 1; i >= 0; i--)
+            {
+                if (b1[i] != b2[i])
+                    return false;
+            }
+
+            return true;
+        }
+
         public static bool SpanEquals(this in Span<byte> span1, in ReadOnlySpan<byte> span2)
         {
             if (span1.Length != span2.Length)
